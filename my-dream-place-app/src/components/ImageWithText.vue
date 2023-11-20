56402628455d463fe9f2df3ff5d656adbd686fad
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-11-20 23:51:15 +0200

    add inspiration

---
+++ my-dream-place-app/src/components/ImageWithText.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative">
+    <img :src="imgPath" alt="Image of a place" />
+    <div>
+      <p
+        class="absolute font-semibold text-xl text-white mx-5 "
+        style="bottom: 74px;  width: 360px"
+      >
+        {{ imgName }}
+      </p>
+      <p
+        class="absolute text-sm text-white mx-5"
+        style="top: 216px; width: 360px"
+      >
+        {{ imgProperties }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { imgName, imgProperties, imgPath } = defineProps([
+  "imgName",
+  "imgProperties",
+  "imgPath",
+]);
+</script>
+
+<style></style>
--- my-dream-place-app/src/components/Inspiration.vue
+++ my-dream-place-app/src/components/Inspiration.vue
@@ -7,10 +7,29 @@
       >
         Get inspiration for your next trip
       </p>
+      <div class="flex gap-5">
+        <ImageWithText
+          :imgPath="require('@/assets/Imgs/sydeny.png')"
+          :imgName="'Sydeny’s 10 most fashionable 5 star hotels'"
+          :imgProperties="'Browse the fastest growing tourism sector in the heart of Australia tourism capital ....'"
+        />
+        <ImageWithText
+          :imgPath="require('@/assets/Imgs/vegan.png')"
+          :imgName="'Top cities for Vegan Travellers'"
+          :imgProperties="'Top sites where you do not have to worry about being a vegan. Our tourist guide is here...'"
+        />
+        <ImageWithText
+          :imgPath="require('@/assets/Imgs/top.png')"
+          :imgName="'World’s top destinations during and post covid timeline'"
+          :imgProperties="'Pandemic is still intact and will be here for a longer time. Here’s where your next destination...'"
+        />
+      </div>
     </section>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import ImageWithText from "./ImageWithText.vue";
+</script>
 
 <style></style>
