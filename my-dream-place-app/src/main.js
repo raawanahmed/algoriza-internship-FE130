67b0f906f9b5d70d2a4c59c00e05a08949dc9cc3
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-11-21 01:08:48 +0200

    install pinia

--- my-dream-place-app/src/main.js
+++ my-dream-place-app/src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './index.css'
+// main.js
 
-createApp(App).use(router).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './index.css';
+import {createPinia} from 'pinia' // Import your Pinia store instance
+
+const app = createApp(App);
+app.use(createPinia()) // Use Pinia
+
+app.use(router);
+app.mount('#app');
