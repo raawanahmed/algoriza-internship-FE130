f87a389bb060bfd619cfa85693b9d9010ce65b7e
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-11-20 20:30:18 +0200

    add covid alert component

---
+++ my-dream-place-app/src/components/CovidAlert.vue
@@ -0,0 +1,24 @@
+<template>
+  <section
+    class="flex rounded-lg h-16 items-center p-4 gap-4"
+    style="
+      margin-left: 100px;
+      margin-right: 100px;
+      margin-top: 75px;
+      margin-bottom: 50px;
+      max-width: 1440px;
+      background: #fcefca;
+      width: 1240px;
+    "
+  >
+    <img src="../assets/Icons/danger.svg" alt="danger icon" />
+    <div class="flex gap-1">
+      <p>Check the latest COVID-19 restrictions before you travel.</p>
+      <a href="#learnmore" class="text-blue-500 hover:underline">Learn more</a>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style></style>
