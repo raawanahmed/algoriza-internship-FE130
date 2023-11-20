13b41123921b7d58e3a1bf41d03bfb2d74ee9084
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-11-20 16:28:06 +0200

    add the img with texts over it

---
+++ my-dream-place-app/src/components/VacationDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mx-auto relative" style="max-width: 1440px">
+    <img
+      src="../assets/Imgs/view.png"
+      alt="view of the sea" 
+      style="margin-left: 100px; margin-right: 100px"
+      class="mt-3"
+    />
+    <p class="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center font-sans text-3xl font-bold" style="top: 202px; width: 610px; height: 38px">
+      Enjoy Your Dream Vacation
+    </p>
+    <p class="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center font-sans text-xl mt-5 font-normal" style="top: 260px; width: 650px; height: 56px">
+        Plan and book our perfect trip with expert advice, travel tips,
+      destination information and inspiration from us
+    </p>
+
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
--- my-dream-place-app/src/views/HomeView.vue
+++ my-dream-place-app/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home"></div>
   <Header></Header>
+  <VacationDetails />
 </template>
 
 <script setup>
-import Header from '@/components/Header.vue';
+import Header from "@/components/Header.vue";
+import VacationDetails from "@/components/VacationDetails.vue";
 </script>
