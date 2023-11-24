3f20d9246c96f202191ad32df7254a451e0174de
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-11-24 21:14:26 +0200

    get where are you going from api

--- my-dream-place-app/src/components/IconButton.vue
+++ my-dream-place-app/src/components/IconButton.vue
@@ -1,20 +1,28 @@
 <template>
   <button :style="{ background: bgOfBtn }" :class="btnClass">
-    <img v-if="iconPath != -undefined" :src="iconPath" alt="icon" />
+    <img v-if="iconPath != undefined" :src="iconPath" alt="icon" />
     <span
-      class="overflow-hidden  text-xs text-gray-2 mt-2 mb-2 font-normal"
+      class="overflow-hidden text-xs text-gray-2 mt-2 mb-2 font-normal"
       style="color: #4f4f4f"
       >{{ textOnTheBtn }}</span
     >
+    <img
+      v-if="arrowDir != undefined"
+      :src="arrowDir"
+      alt="arrow"
+      class="w-4 h-4 absolute"
+      style="right: 8px"
+    />
   </button>
 </template>
 
 <script setup>
-const { textOnTheBtn, bgOfBtn, btnClass, iconPath } = defineProps([
+const { textOnTheBtn, bgOfBtn, btnClass, iconPath, arrowDir } = defineProps([
   "textOnTheBtn",
   "bgOfBtn",
   "iconPath",
   "btnClass",
+  "arrowDir",
 ]);
 </script>
 
--- my-dream-place-app/src/components/SearchSection.vue
+++ my-dream-place-app/src/components/SearchSection.vue
@@ -3,12 +3,31 @@
     class="absolute left-1/2 transform -translate-x-1/2 -translate-y-10 bg-white rounded-lg shadow-md p-3 mt-8 gap-3.5 flex fontStyle"
     style="width: 1030px; height: 64px; top: 480px"
   >
-    <IconButton
-      :textOnTheBtn="'Where are you going?'"
-      :btnClass="'p-3 rounded h-11 w-52 flex items-center font-normal gap-1.5'"
-      :bgOfBtn="'#F2F2F2'"
-      :iconPath="require('@/assets/Icons/location.svg')"
-    />
+    <div>
+      <IconButton
+        :textOnTheBtn="selectedDestinationName"
+        :btnClass="'p-3 rounded h-11 w-52 flex items-center font-normal gap-1.5 relative'"
+        :bgOfBtn="'#F2F2F2'"
+        :iconPath="require('@/assets/Icons/location.svg')"
+        :arrowDir="require(`@/assets/Icons/arrow-${arrowDir}.svg`)"
+        @click="toggleDropdown"
+      />
+      <div
+        v-if="isDropdownOpen"
+        class="absolute mt-1 w-52 bg-white shadow-md rounded-md text-xs"
+        style="background: #f2f2f2"
+      >
+        <div v-for="destination in destinations" :key="destination.dest_id">
+          <button
+            @click="selectDestination(destination)"
+            class="block w-full p-2 border-b-2"
+          >
+            {{ destination.label }}
+          </button>
+        </div>
+      </div>
+    </div>
+
     <IconButton
       :textOnTheBtn="'Check in date'"
       :btnClass="'p-3 rounded h-11 w-36 flex items-center font-normal gap-1.5'"
@@ -42,7 +61,49 @@
 </template>
 
 <script setup>
+import { ref } from "vue";
 import IconButton from "./IconButton.vue";
+import axios from "axios";
+
+const isDropdownOpen = ref(false);
+const arrowDir = ref("down");
+const selectedDestinationName = ref("Where are you going?");
+const destinations = ref([]);
+
+const fetchDestinations = async () => {
+  const options = {
+    method: "GET",
+    url: "https://booking-com15.p.rapidapi.com/api/v1/hotels/searchDestination",
+    params: { query: "Egypt" },
+    headers: {
+      "X-RapidAPI-Key": "5207885cbdmshf5ef7933079e239p1299b7jsn81996ac3e431",
+      "X-RapidAPI-Host": "booking-com15.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    destinations.value = response.data.data;
+    console.log(destinations.value);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const toggleDropdown = () => {
+  isDropdownOpen.value = !isDropdownOpen.value;
+  arrowDir.value == "down"
+    ? (arrowDir.value = "up")
+    : (arrowDir.value = "down");
+  console.log(isDropdownOpen.value);
+};
+
+const selectDestination = (destination) => {
+  selectedDestinationName.value = destination.label;
+  isDropdownOpen.value = false;
+};
+
+fetchDestinations();
 </script>
 
 <style>
