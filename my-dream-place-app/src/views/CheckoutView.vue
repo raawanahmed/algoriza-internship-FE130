9754a4e1680cbbaf293c358c37bc34c7019e722a
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-11-25 01:46:14 +0200

    style checkout page

--- my-dream-place-app/src/views/CheckoutView.vue
+++ my-dream-place-app/src/views/CheckoutView.vue
@@ -4,10 +4,19 @@
     :notificationColor="'notification-black'"
     :showNavBar="false"
   />
+  <main style="background: #f4f4f4; height: 1820px">
+    <div style="max-width: 1440px" class="w-full flex mx-auto font-semibold">
+      <p style="margin-left: 100px; font-size: 32px; margin-top: 60px">
+        Secure your reservation
+      </p>
+    </div>
+    <CovidAlert />
+  </main>
 </template>
 
 <script setup>
 import Header from "@/components/Header.vue";
+import CovidAlert from "@/components/CovidAlert.vue";
 </script>
 
 <style></style>
