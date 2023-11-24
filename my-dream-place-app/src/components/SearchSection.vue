87fa2f76e71566552c2c310a54b514b01ca71671
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-11-24 23:11:48 +0200

    add vue datetime picker

--- my-dream-place-app/src/components/SearchSection.vue
+++ my-dream-place-app/src/components/SearchSection.vue
@@ -28,18 +28,42 @@
       </div>
     </div>
 
-    <IconButton
+    <!-- <IconButton
       :textOnTheBtn="'Check in date'"
       :btnClass="'p-3 rounded h-11 w-36 flex items-center font-normal gap-1.5'"
       :bgOfBtn="'#F2F2F2'"
       :iconPath="require('@/assets/Icons/calendarIcon.svg')"
-    />
-    <IconButton
-      :textOnTheBtn="'Check out date'"
-      :btnClass="'p-3 rounded h-11 w-36 flex items-center font-normal gap-1.5'"
-      :bgOfBtn="'#F2F2F2'"
-      :iconPath="require('@/assets/Icons/calendarIcon.svg')"
-    />
+    /> -->
+    <div
+      class="flex items-center rounded"
+      style="width: 147px; height: 43px; color: #4f4f4f; background: #f2f2f2"
+    >
+      <img src="@/assets/Icons/calendarIcon.svg" alt="" class="w-5 h-5 ml-3" />
+      <VueDatePicker
+        v-model="checkInDate"
+        :placeholder="checkInDate != null ? checkInDate.value : 'Check in date'"
+        class="p-2 rounded flex items-center font-normal gap-1.5"
+        @click="getCheckIn"
+        hide-input-icon
+        :enable-time-picker="false"
+      />
+    </div>
+    <div
+      class="flex items-center rounded"
+      style="width: 147px; height: 43px; color: #4f4f4f; background: #f2f2f2"
+    >
+      <img src="@/assets/Icons/calendarIcon.svg" alt="" class="w-5 h-5 ml-3" />
+      <VueDatePicker
+        v-model="checkOutDate"
+        :placeholder="
+          checkOutDate != null ? checkOutDate.value : 'Check out date'
+        "
+        class="p-2 rounded flex items-center font-normal gap-1.5"
+        @click="getCheckOut"
+        hide-input-icon
+        :enable-time-picker="false"
+      />
+    </div>
     <IconButton
       :textOnTheBtn="'Guests'"
       :btnClass="'p-3 rounded h-11 w-36 flex items-center font-normal gap-1.5'"
@@ -54,6 +78,7 @@
     />
     <button
       class="inline-flex px-10 py-2 justify-center items-center gap-2 rounded border border-blue-500 bg-blue-500 text-white w-36 h-11 font-medium text-sm"
+      @click="goSearch"
     >
       Search
     </button>
@@ -64,11 +89,15 @@
 import { ref } from "vue";
 import IconButton from "./IconButton.vue";
 import axios from "axios";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
 
 const isDropdownOpen = ref(false);
 const arrowDir = ref("down");
 const selectedDestinationName = ref("Where are you going?");
 const destinations = ref([]);
+const checkInDate = ref(null);
+const checkOutDate = ref(null);
 
 const fetchDestinations = async () => {
   const options = {
@@ -84,7 +113,7 @@ const fetchDestinations = async () => {
   try {
     const response = await axios.request(options);
     destinations.value = response.data.data;
-    console.log(destinations.value);
+    // console.log(destinations.value);
   } catch (error) {
     console.error(error);
   }
@@ -97,18 +126,39 @@ const toggleDropdown = () => {
     : (arrowDir.value = "down");
   console.log(isDropdownOpen.value);
 };
-
 const selectDestination = (destination) => {
   selectedDestinationName.value = destination.label;
   isDropdownOpen.value = false;
 };
-
+const getCheckIn = () => {
+  console.log(checkInDate.value);
+  return checkInDate.value;
+};
+const getCheckOut = () => {
+  console.log(checkOutDate.value);
+  return checkOutDate.value;
+};
+const goSearch = () => {};
 fetchDestinations();
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&family=Work+Sans:wght@100;400&display=swap");
 .fontStyle {
-  font-family: "Poppins", sans-serif;
+  font-family: "Work Sans", sans-serif;
+}
+
+.dp__pointer.dp__input_readonly.dp__input.dp__input_reg {
+  border: 0px;
+  background: #f2f2f2;
+  font-size: 12px;
+  padding: 0px;
+  color: #4f4f4f;
+  font-family: "Work Sans", sans-serif;
+}
+.dp__pointer.dp__input_readonly.dp__input.dp__input_reg::placeholder {
+  color: #4f4f4f;
+  opacity: 1;
+  font-family: "Work Sans", sans-serif;
 }
 </style>
--- my-dream-place-app/src/main.js
+++ my-dream-place-app/src/main.js
@@ -4,9 +4,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './index.css';
-import {createPinia} from 'pinia' // Import your Pinia store instance
+import {createPinia} from 'pinia' 
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 const app = createApp(App);
+app.component('VueDatePicker', VueDatePicker);
 app.use(createPinia()) // Use Pinia
 
 app.use(router);
