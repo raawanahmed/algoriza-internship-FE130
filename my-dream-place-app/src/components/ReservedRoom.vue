72c389db985fd776bec63e0f4b6a4192f722e62a
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-11-30 16:50:13 +0200

    add reserved room component

---
+++ my-dream-place-app/src/components/ReservedRoom.vue
@@ -0,0 +1,43 @@
+<template>
+  <main :class="classOfTag" class="bg-white" style="font-family: SF Pro Display, sans-serif">
+    <img src="../assets/Imgs/aust.png" alt="room image" />
+    <div>
+      <div class="ml-6 mb-[25px]">
+        <p class="font-medium text-lg pt-10 text-[#1a1a1a]">
+          {{ selectedHotel.property.name }}
+        </p>
+        <section class="flex mt-3 mb-3">
+          <Reviews
+            :reviewScore="selectedHotel.property.reviewScore"
+            :reviewsCount="selectedHotel.property.reviewCount"
+          />
+        </section>
+      </div>
+      <div class="ml-6 mb-5">
+        <p class="mb-2">Non refundable</p>
+        <p class="mb-2">Check in: Sunday, March 18, 2022</p>
+        <p class="mb-2">Check in: Sunday, March 18, 2022</p>
+        <p class="mb-2">Check out: Tuesday, March 20, 2022</p>
+        <p>2 night stay</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useHotelStore } from "@/stores/HotelStore";
+import Reviews from "./Reviews.vue";
+
+const { classOfTag, hotelData, roomData } = defineProps([
+  "classOfTag",
+  "hotelData",
+  "roomData",
+]);
+
+const hotelStore = useHotelStore();
+const selectedHotel = ref(hotelStore.getselectedHotelData);
+const isLoading = ref(false);
+</script>
+
+<style></style>
--- my-dream-place-app/src/views/CheckoutView.vue
+++ my-dream-place-app/src/views/CheckoutView.vue
@@ -1,17 +1,19 @@
-<template >
+<template>
   <Header
     :textColor="'black'"
     :notificationColor="'notification-black'"
     :showNavBar="false"
   />
-  <main class="bg-[#f4f4f4] pb-[140px]">
+  <main class="bg-[#f4f4f4] pb-[140px]" >
     <div class="w-full mx-auto max-w-[1440px]">
       <p class="mx-[100px] text-[32px] font-semibold pt-[60px]">
         Secure your reservation
       </p>
       <CovidAlert class="mt-[30px]" />
-
-      <PaymentDetails />
+      <div class="flex">
+        <PaymentDetails class="mr-[30px]"/>
+        <ReservedRoom :classOfTag="'w-[400px] h-[400px]'" />
+      </div>
     </div>
   </main>
 </template>
@@ -20,6 +22,7 @@
 import Header from "@/components/Header.vue";
 import CovidAlert from "@/components/CovidAlert.vue";
 import PaymentDetails from "@/components/PaymentDetails.vue";
+import ReservedRoom from "@/components/ReservedRoom.vue";
 </script>
 
 <style></style>
