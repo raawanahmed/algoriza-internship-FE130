7772250e66504915fab99d1bd10941c0a9e08fdc
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-12-03 00:33:09 +0200

    add 3 quotas for testing the project

--- my-dream-place-app/src/config.js
+++ my-dream-place-app/src/config.js
@@ -1,13 +1,11 @@
 // config.js
 const config = {
-  rapidApiKey: "d781e6178cmsh2afa53aff755cc1p1ac0bcjsn805975a9fc0b",
+  rapidApiKey: "30bd072d35msh472060fab12beebp123a35jsn3aaa2f4c393f",
 };
 /*
 rapidApiKeys from my all accounts to replace them when quota finished
-9077d7cc36mshe3cc1da9b9c21b0p1248a2jsn3415ae078077
-
-
-
-
+30bd072d35msh472060fab12beebp123a35jsn3aaa2f4c393f
+eba504b5a7mshad81b29a4cdb842p182174jsna1c22abcde25
+c0f2fe4194msh85ea38e6fd09b99p1b3141jsnbe052206c6d1
 */
 export default config;
