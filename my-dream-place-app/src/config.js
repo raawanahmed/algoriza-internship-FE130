fa57c69af28ce56f2fd92d4abe535537e8c38371
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-12-03 15:07:30 +0200

    update config file with rapidApiKeys

--- my-dream-place-app/src/config.js
+++ my-dream-place-app/src/config.js
@@ -1,11 +1,11 @@
 // config.js
 const config = {
-  rapidApiKey: "30bd072d35msh472060fab12beebp123a35jsn3aaa2f4c393f",
+  rapidApiKey: "203e72ca3dmsh7e333b34714a8b8p1d1980jsn972bce7e0f98",
 };
 /*
-rapidApiKeys from my all accounts to replace them when quota finished
-30bd072d35msh472060fab12beebp123a35jsn3aaa2f4c393f
-eba504b5a7mshad81b29a4cdb842p182174jsna1c22abcde25
+rapidApiKeys to replace them when quota finished
+6b93e1e946mshe4e42335df99f46p1b26bejsnde24847dde94
 c0f2fe4194msh85ea38e6fd09b99p1b3141jsnbe052206c6d1
+bece71ea60msh51a3282cd69267bp10e275jsn2721424cea73
 */
 export default config;
